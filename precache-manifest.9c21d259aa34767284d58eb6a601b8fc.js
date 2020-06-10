8f27677fdc744fda42eb58f0510604e79c5266a7
Luan Quach <quachminhluantn@gmail.com>
2020-06-11 00:22:12 +0800

    Updates

---
+++ precache-manifest.9c21d259aa34767284d58eb6a601b8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a8ab3af1b83271bafa84e6cbcdd8785",
+    "url": "/RoboFriend/index.html"
+  },
+  {
+    "revision": "9502d92cffa86ad8c984",
+    "url": "/RoboFriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9502d92cffa86ad8c984",
+    "url": "/RoboFriend/static/js/2.fc4af8c5.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/RoboFriend/static/js/2.fc4af8c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fee80865f4f0a8d3dcf",
+    "url": "/RoboFriend/static/js/main.d62ee5d6.chunk.js"
+  },
+  {
+    "revision": "2e0ef8019e1df75085bf",
+    "url": "/RoboFriend/static/js/runtime-main.dc976db2.js"
+  }
+]);
